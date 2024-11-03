4499c3bb2d042bb6cdf6b49d90a217da621e3357
theNareynater <whitacre@purdue.edu>
2024-11-03 04:10:09 -0500

    The third dimension bouta go hella crazy

--- Assets/Scripts/ZJump.cs
+++ Assets/Scripts/ZJump.cs
@@ -9,9 +9,13 @@ public class ZJump : MonoBehaviour
 {
 
     [SerializeField] private bool startNear = false;
+    [SerializeField] private float alphaHigh = 1.0f;
+    [SerializeField] private float alphaLow = 0.3f;
+    [SerializeField] private float alphaFadeTime = 0.3f;
+    [SerializeField] private float alphaFadeTickRate = 0.025f;
     private Transform player;
-    private Transform mainCamera;
     private bool isNear;
+    private bool canZJump = false;
 
     public List<SpriteRenderer> farRenderers;
     public List<SpriteRenderer> nearRenderers;
@@ -28,21 +32,24 @@ public class ZJump : MonoBehaviour
         allRenderers = FindObjectsOfType(typeof(SpriteRenderer));
         foreach (SpriteRenderer renderer in allRenderers)
         {
-            if (renderer.gameObject.tag == "Player") continue;
-            if (renderer.gameObject.transform.localToWorldMatrix.GetPosition().z > -5f)
+            if (renderer.gameObject.tag == "Player" || renderer.gameObject.tag == "StaticSprite") continue;
+
+            if (renderer.gameObject.transform.localToWorldMatrix.GetPosition().z > -5f &&
+                renderer.gameObject.transform.localToWorldMatrix.GetPosition().z < 5f)
             {
                 farRenderers.Add(renderer);
+                continue;
             }
-            else
+            if (renderer.gameObject.transform.localToWorldMatrix.GetPosition().z > -15f &&
+                renderer.gameObject.transform.localToWorldMatrix.GetPosition().z < -5f)
             {
                 nearRenderers.Add(renderer);
             }
         }
-        //FindObjectsOfType
         isNear = startNear;
         player = GameObject.FindGameObjectWithTag("Player").transform;
-        mainCamera = GameObject.FindGameObjectWithTag("CameraHolder").transform;
-        setSpriteAlphas();
+        setSpriteAlphas(1f);
+        canZJump = true;
     }
 
     // Update is called once per frame
@@ -63,33 +70,46 @@ public class ZJump : MonoBehaviour
 
     public void zJump()
     {
+        if (!canZJump) return;
+
         if (isNear)
         {
             //JumpFar
             isNear = false;
             player.position = player.position + new Vector3(0f, 0f, 10f);
-            //mainCamera.position = mainCamera.position + new Vector3(0f, 0f, 20f);
         } else
         {
             //JumpNear
             isNear = true;
             player.position = player.position + new Vector3(0f, 0f, -10f);
-            //mainCamera.position = mainCamera.position + new Vector3(0f, 0f, -20f);
         }
-        setSpriteAlphas();
+        StartCoroutine(fadeSpriteAlphas());
     }
 
 
-    public void setSpriteAlphas()
+    public void setSpriteAlphas(float fadePercentage)
     {
+        float highFadeAmount = alphaLow + ((alphaHigh - alphaLow) * fadePercentage);
+        float lowFadeAmount = alphaHigh - ((alphaHigh - alphaLow) * fadePercentage);
         foreach (SpriteRenderer renderer in nearRenderers)
         {
-            //float alpha = (isNear) ? 0.5
-            renderer.color = new Color(renderer.color.r, renderer.color.g, renderer.color.b, ((isNear) ? 1f : 0.3f));
+            renderer.color = new Color(renderer.color.r, renderer.color.g, renderer.color.b, ((isNear) ? highFadeAmount : lowFadeAmount));
         }
         foreach (SpriteRenderer renderer in farRenderers)
         {
-            renderer.color = new Color(renderer.color.r, renderer.color.g, renderer.color.b, ((!isNear) ? 1f : 0.3f));
+            renderer.color = new Color(renderer.color.r, renderer.color.g, renderer.color.b, ((!isNear) ? highFadeAmount : lowFadeAmount));
         }
+    }
+
+
+    private IEnumerator fadeSpriteAlphas()
+    {
+        canZJump = false;
+        for (int i = 1; i <= alphaFadeTime / alphaFadeTickRate; i++)
+        {
+            setSpriteAlphas((alphaFadeTickRate / alphaFadeTime) * i);
+            yield return new WaitForSeconds(alphaFadeTickRate);
+        }
+        canZJump = true;
     }
 }
