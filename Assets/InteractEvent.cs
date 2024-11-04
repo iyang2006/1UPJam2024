ba3da8fc0da0b507620a4dc5809112ed9496bad7
theNareynater <whitacre@purdue.edu>
2024-11-03 19:53:15 -0500

    level 1 wowie zowie

--- Assets/InteractEvent.cs
+++ Assets/InteractEvent.cs
@@ -8,6 +8,7 @@ public class InteractEvent : MonoBehaviour
     [SerializeField] private InteractUse interactUse;
     [SerializeField] private SpriteRenderer doorRenderer;
     [SerializeField] private GameObject doorWay;
+    [SerializeField] private ZomMove zombie;
     private bool isCompleted = false;
 
     // Start is called before the first frame update
@@ -15,6 +16,7 @@ public class InteractEvent : MonoBehaviour
     {
         doorRenderer.enabled = false;
         doorWay.SetActive(false);
+        zombie.enabled = false;
     }
 
     // Update is called once per frame
@@ -32,5 +34,13 @@ public class InteractEvent : MonoBehaviour
         isCompleted = true;
         doorRenderer.enabled = true;
         doorWay.SetActive(true);
+        //zombie.enabled = true;
+        StartCoroutine(activeZombie());
+    }
+
+    private IEnumerator activeZombie()
+    {
+        yield return new WaitForSeconds(4f);
+        zombie.enabled = true;
     }
 }
